86cf6766584c62fb8b48d8df2c34e1d9c187bf3b
Angelo Pirola <angelo9871@gmail.com>
2023-03-10 17:53:42 +0100

    Aggiunta dei commenti sulla documentazione dell'interfaccia

--- src/SerilogCustomExtensions/Service/IUtilService.cs
+++ src/SerilogCustomExtensions/Service/IUtilService.cs
@@ -2,6 +2,19 @@
 
 public interface IUtilService
 {
+    /// <summary>
+    /// Abstraction of the objectResult of the statusCode
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="statusCode"></param>
+    /// <param name="typeCode"></param>
+    /// <param name="httpContext"></param>
+    /// <returns></returns>
     ErrorResponse ManageError(string message, int statusCode, int typeCode, HttpContext httpContext);
+
+    /// <summary>
+    /// Saving logs of type information
+    /// </summary>
+    /// <param name="message"></param>
     void SaveLogInformation(string message);
 }
