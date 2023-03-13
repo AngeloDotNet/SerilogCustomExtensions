994f074446b694e16ee2ef0e41c44971533551c7
Angelo Pirola <angelo9871@gmail.com>
2023-03-13 20:38:42 +0100

    Refactoring

--- src/SerilogCustomExtensions/GlobalUsings.cs
+++ src/SerilogCustomExtensions/GlobalUsings.cs
@@ -1,3 +1,7 @@
-﻿global using Microsoft.AspNetCore.Http;
+﻿global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Logging;
+global using Serilog;
 global using SerilogCustomExtensions.Models;
+global using SerilogCustomExtensions.Service;
