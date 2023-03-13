83d8249e5bd32c8a7953778b3991e4f8a5e16a0a
Angelo Pirola <angelo9871@gmail.com>
2023-03-13 20:38:26 +0100

    Aggiunti nuovi metodi statici

---
+++ src/SerilogCustomExtensions/Extentions/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿namespace SerilogCustomExtensions.Extentions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddBusinessLayerServices(this IServiceCollection services)
+    {
+        services
+            .AddTransient<IUtilService, UtilService>();
+
+        return services;
+    }
+
+    public static WebApplication AddApplicationServices(this WebApplication application)
+    {
+        application.UseSerilogRequestLogging(options =>
+        {
+            options.IncludeQueryInRequestPath = true;
+        });
+
+        return application;
+    }
+
+    public static WebApplicationBuilder AddOptionsBuilder(this WebApplicationBuilder builder)
+    {
+        builder.Host.UseSerilog((hostingContext, loggerConfiguration) =>
+        {
+            loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration);
+        });
+
+        return builder;
+    }
+}
